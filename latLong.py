9c422b1f8d3eb74d3423a4a3ebf7dc3317010532
copperstick6 <copperstick6@gmail.com>
2016-10-29 10:53:58 -0500

    added function that auto gets uset location

---
+++ latLong.py
@@ -0,0 +1,11 @@
+def getLatLong():
+    send_url = 'http://freegeoip.net/json'
+    webUrl = urllib.request.urlopen(send_url)
+    if (webUrl.getcode() == 200):
+        str_response = webUrl.read().decode('utf-8')
+        obj = json.loads(str_response)
+        a = []
+        a.append(obj["latitude"])
+        a.append(obj["longitude"])
+    return a
+print(getLatLong())
